6d9539931ff8ee682f2235dc4983031dd3e6cc95
Joao Pereira <joaopapereira@gmail.com>
2018-04-17 08:36:25 -0400

    remove not needed code

--- src/mocha-react-css-modules.js
+++
@@ -1,7 +0,0 @@
-import hook from 'css-modules-require-hook';
-import sass from 'node-sass';
-
-hook({
-  extensions: ['.scss'],
-  preprocessCss: data => sass.renderSync({data}).css
-});
--- src/testSetup.js
+++ src/testSetup.js
@@ -1,9 +1,3 @@
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 Enzyme.configure({ adapter: new Adapter() });
-
-// import jsdom from 'jsdom';
-
-// const document = jsdom.jsdom('<!doctype html><html><body></body></html>');
-// global.document = document;
-// global.window = document.defaultView;
